d2b90d9bd000953533df575b3cb48c3048a596a8
ACnoWA <yi.li.sfeg@gmail.com>
2019-04-29 17:33:49 +0800

    01-vue-experiment

---
+++ vue-examples1/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <myheader />
+    <div id="main">
+      <mysidebar />
+      <myarticle />
+    </div>
+    <myfooter />
+  </div>
+</template>
+<script>
+export default {
+  components: {
+    myheader: () => import("./views/Header"),
+    myfooter: () => import("./views/Footer"),
+    mysidebar: () => import("./views/Sidebar"),
+    myarticle: () => import("./views/Article")
+  }
+};
+</script>
+
+<style>
+#main {
+  margin: 5px 0px;
+  border: 1px solid red;
+}
+</style>
---
+++ vue-examples1/src/views/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <a class="active" href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">News</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li class="right">
+        <a href="#">Logout</a>
+      </li>
+    </ul>
+  </div>
+</template>
+<style scoped>
+div {
+  padding: 5px;
+  border: 1px solid red;
+  margin: 5px 10px;
+}
+ul {
+  display: flex;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  background-color: #333;
+}
+a {
+  display: block; /* 变为block级，填充高宽 */
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+.active {
+  background-color: #4caf50;
+}
+a:hover {
+  background-color: #111;
+}
+.right {
+  margin-left: auto;
+}
+</style>
